b5548088f0bc88995da57cecbd2740bc6105dc49
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-10-14 10:25:00 -0400

    user, friendship, post and comment models generated

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,22 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
+  has_many :friendships
+  has_many :friends, -> { where(friendships: {status: 'accepted' }) },
+           through: :friendships
+  # # has_many :friends, through: :friendships do
+  #   def status
+  #     where("friendships.status = ?", 'accepted')
+  #   end
+  # end
+  has_many :posts
+  has_many :comments
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013200254) do
+ActiveRecord::Schema.define(version: 20161014040756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -31,6 +54,7 @@ ActiveRecord::Schema.define(version: 20161013200254) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
