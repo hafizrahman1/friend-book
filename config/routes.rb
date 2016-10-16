9f0240a2b85ad0c159474ab5ea0ebd58e8bdfe23
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-10-16 11:01:39 -0400

    updated routes for posts and friendships

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  
   root 'users#index'
   resources :users, except: [:edit, :update, :destroy]
+  resources :posts do
+    resources :comments
+  end
+  resources :friendships, only: [:index, :create, :update, :destroy]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
