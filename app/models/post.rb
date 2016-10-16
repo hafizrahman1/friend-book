6632f39735ded32824dff34e85c74c7f8259925b
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-10-16 12:55:59 -0400

    modified post model, added validation of image size and mount_uploader method

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,16 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :commenting_users, through: :comments, source: :user
   has_many :likes
+  has_many :liking_users, through: :likes, source: :user
+  mount_uploader :photo, PhotoUploader
+
+  validate :photo_size
+
+  private
+
+  def photo_size
+    if photo.size > 5.megabyes
+      errors.add(:photo, "Image should be less than 5MB")
+    end
+  end
 end
