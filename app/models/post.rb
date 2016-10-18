e454153b12a4ba922f0729543fef86a8b379a61a
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-10-18 14:01:17 -0400

    Added association and validation on posts and tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,11 @@ class Post < ActiveRecord::Base
   has_many :commenting_users, through: :comments, source: :user
   has_many :likes, dependent: :destroy
   has_many :liking_users, through: :likes, source: :user
+  has_many :post_tags
+  has_many :tags, through: :post_tags
   mount_uploader :photo, PhotoUploader
-
+  
+  validates_presence_of :content
   validate :photo_size
 
   private
