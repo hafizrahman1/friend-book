27842968b70f21b213223b2d9d9fab10b762519d
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-10-18 13:38:41 -0400

    updated the header and layouts

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,47 @@
 class FriendshipsController < ApplicationController
+  before_action :find_friend, except: :index
+
+  def index
+    @friends = current_user.friends
+  end
+
+  def create
+    Friendship.request(current_user, @friend)
+    flash[:notice] = "Friend request send"
+    redirect_to :back 
+  end
+
+  def update
+    if current_user.requested_friends.include?(@friend)
+      Friendship.accept(current_user, @friend)
+      flash[:notice] = "Friendship with #{@friend.name} accepted"
+    else
+      flash[:notice] = "No Friendship request"
+    end
+    redirect_to :back
+  end
+
+  def destroy
+    if current_user.friends.include?(@friend)
+      Friendship.breakup(current_user, @friend)
+      flash[:notice] = "Successfully unfriended #{@friend.name}"
+    elsif current_user.requested_friends.include?(@friend)
+      Friendship.breakup(current_user, @friend)
+      flash[:notice] = "Friendship with #{@friend.name} declined"
+    elsif current_user.pending_friends.include?(@friend)
+      Friendship.breakup(current_user, @friend)
+      flash[:notice] = "Friendship request to #{@friend.name} retracted"
+    else
+      flash[:notice] = "No Friendship request"
+    end
+    redirect_to :back
+
+  end
+
+  private
+
+  def find_friend
+    @friend = User.find(params[:friend_id])
+  end
+
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,35 @@ class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: "User", foreign_key: "friend_id"
 
+  validates_presence_of :user_id, :friend_id
+
+  def self.exists?(user, friend)
+    not find_by_user_id_and_friend_id(user, friend).nil?
+  end
+
+  def self.request(user, friend)
+    unless user == friend or Friendship.exists?(user, friend)
+      Friendship.create(user: user, friend: friend, status: 'pending')
+      Friendship.create(user: friend, friend: user, status: 'requested')
+    end
+  end
+
+  def self.accept(user, friend)
+    Friendship.accept_half(user, friend)
+    Friendship.accept_half(friend, user)
+  end
+
+  def self.breakup(user, friend)
+    destroy(find_by_user_id_and_friend_id(user, friend))
+    destroy(find_by_user_id_and_friend_id(friend, user))
+  end
+
+  private
+
+  def self.accept_half(user, friend)
+    request = find_by_user_id_and_friend_id(user, friend)
+    request.status = 'accepted'
+    request.save
+  end
+
 end
